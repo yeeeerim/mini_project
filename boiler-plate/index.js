57f43a79e0e17fb408a09897eb482ce8e410f592
yerim <pppp5097@naver.com>
2021-06-19 22:25:33 +0900

    <inflean> React 기본
    .

---
+++ boiler-plate/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://yerim:q1w2@boilerplate.pbd8y.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=>console.log('MongoDB Connected...')).catch(err => console.log(err))
+
+app.get('/', (req,res) => res.send('Hello World!!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
